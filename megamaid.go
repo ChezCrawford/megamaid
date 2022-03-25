cf91c5326ebd7fc52b9dad0d30924a6f30e7ff21
Charlie Crawford <ccrawford@pagerduty.com>
2022-03-25 08:50:37 -0400

    Simple program args.

--- megamaid.go
+++ megamaid.go
@@ -1,26 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Starting MegaMaid...")
-	refreshFeed()
-	loadFeed()
-	// downloadEpisode()
+
+	cmd := os.Args[1]
+	switch cmd {
+	case "refresh":
+		refreshFeed()
+	case "view":
+		loadFeed()
+	case "download":
+		indexInput := os.Args[2]
+		itemIndex, _ := strconv.Atoi(indexInput)
+		downloadEpisode(itemIndex)
+	default:
+		fmt.Printf("I do not know how to '%s' yet\n", cmd)
+	}
+
 	fmt.Println("Shutting down MegaMaid...")
 }
 
-func downloadEpisode() {
-	itemIndex := 0
+func downloadEpisode(itemIndex int) {
 	itemsToDownload := 1
 	feed, _ := LoadFeed("the-anjunadeep-edition.xml")
 
-	for itemIndex < itemsToDownload {
-		item, _ := feed.GetItemAtIndex(itemIndex)
+	for i := itemIndex; i < (itemIndex + itemsToDownload); i++ {
+		item, _ := feed.GetItemAtIndex(i)
 		name := item.GetFileName(feed)
 		url := item.Enclosure.Url
 		DownloadFile(url, name)
-		itemIndex++
 	}
 }
 
